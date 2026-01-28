09b5fbcdc99f1e84e0a67d913c9bd93ed87489fa
JMeep2007 <meepegamajulian1@gmail.com>
2026-01-28 12:43:11 -0800

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by MEEPEGAMA, JULIAN on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
